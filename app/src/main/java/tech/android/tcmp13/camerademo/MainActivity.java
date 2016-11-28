7dbe04919dae11b3ef576b30b18c723a2b865822
Alex <alexb@upmaster.co.il>
2016-11-28 16:28:18 +0200

    The project form Noy

---
+++ app/src/main/java/tech/android/tcmp13/camerademo/MainActivity.java
@@ -0,0 +1,68 @@
+package tech.android.tcmp13.camerademo;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.camera:
+                boolean hasCamera = getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+                if (!hasCamera) {
+                    Toast.makeText(this, "You have no camera", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) == null) {
+                    Toast.makeText(this, "You have no camera APP", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                startActivityForResult(intent, 169);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        //DONT DARE TO DO DIS
+        if (requestCode != 169)
+            return;
+        if (resultCode != RESULT_OK)
+            return;
+        //"data" is the key provided by the android native camera app, maybe other apps use different key!!!
+        Bundle extras = data.getExtras();
+        Bitmap resultImage = (Bitmap) extras.get("data");
+        imageView.setImageBitmap(resultImage);
+    }
+}
